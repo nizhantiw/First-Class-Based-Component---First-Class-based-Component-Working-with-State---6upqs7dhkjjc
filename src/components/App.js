049157985f67538f05e526f1252bbbadfadc8d45
nizhantiw <nizhantiw@gmail.com>
2022-12-12 19:00:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,29 @@
-import React from 'react'
+import React, {Component} from 'react'
 import '../styles/App.css';
 
+class App extends Component {
+ constructor(props){
+    super(props)
+    this.state={name: "John Doe", enrollmentNo: "12345678", age: 34}
+ }
 
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-// write your code here
-      () => {
-        this.tick()
-      },1000
- 
-    );
-  }
-
-  tick() {
+ handleButtonClick() {
     this.setState({
-      date: new Date()
-    });
-    
-  }
+    age:this.state.age+1
+    })
+ }
 
-  render() {
-    return (
-      <div id='main'>
-        <h1>Welcome to Newton School</h1>
-        <div id='timer'>{this.state.date.toLocaleTimeString()}</div>
-      </div>
-    );
-  }
+
+    render() {
+        return (
+            <>
+                <h1>{`Hello, my name is ${this.state.name}`}</h1>
+                <p>{`I am ${this.state.age} years old and my enrollment no is ${this.state.enrollmentNo}`}</p>
+                <button onClick={()=>this.handleButtonClick()}>Submit</button>
+            </>
+        )
+    } 
 }
-
 
 
 export default App;
